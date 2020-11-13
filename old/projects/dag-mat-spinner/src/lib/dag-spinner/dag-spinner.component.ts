892ed7d7d80b4094c43542730432846b4201df11
ilDug <dognini.marco@gmail.com>
2020-11-13 15:55:14 +0100

    bug fixed

--- old/projects/dag-mat-spinner/src/lib/dag-spinner/dag-spinner.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'dag-dag-spinner',
-  templateUrl: './dag-spinner.component.html',
-  styleUrls: ['./dag-spinner.component.scss']
-})
-export class DagSpinnerComponent {
-}
--- old/projects/dag-mat-spinner/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of dag-mat-spinner
- */
-
-export * from './lib/dag-mat-spinner.module';
-export * from './lib/dag-spinner.directive';
